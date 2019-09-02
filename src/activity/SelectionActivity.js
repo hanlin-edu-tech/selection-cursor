2a31af4ec970035434b22e235c1d6650e19f9ffb
Sparrow.jang <sparrow.jang@gmail.com>
2019-09-02 12:37:00 +0800

    Added a adapter to activity.

--- src/activity/SelectionActivity.js
+++ src/activity/SelectionActivity.js
@@ -1,32 +1,51 @@
 
 import Selection from './../Selection';
-import Cursor from './../Cursor';
-
+import MODE from './../mode/index';
 
 class SelectionActivity {
 
-  constructor(el) {
+  constructor(el, adapter) {
     this.el = el;
+    this.adapter = adapter;
+
+    this.mode = MODE.STANDARD;
+    this.keyModeMap = this.initKeyModeMap();
 
     this.selection = new Selection();
+    this.adapter.setActivity(this);
     this.registerEvent();
+    this.adapter.init();
+  }
+
+  initKeyModeMap() {
+    const map = new Map();
+
+    map.set(16, MODE.CONTINUATION);
+    map.set(17, MODE.OPTION);
+    map.set(91, MODE.OPTION);
+
+    return map;
   }
 
   registerEvent() {
-    let el = this.el;
-
-    el.addEventListener('click', (evt)=> {
-      console.log(evt);
-      let cursor = new Cursor(evt.target);
-      this.selection.setCursor(cursor);
-    });
 
     window.addEventListener('keydown', (evt)=> {
+
+      let availableMode = this.keyModeMap.get(evt.which);
+
+      if (availableMode) {
+        this.mode = availableMode;
+      }
+
       console.log(evt);
     });
 
     window.addEventListener('keyup', (evt)=> {
 
+      if (this.keyModeMap.has(evt.which)) {
+        this.mode = MODE.STANDARD;
+      }
+      console.log(evt);
     });
 
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
 import './style/index.less';
 import SelectionActivity from './activity/SelectionActivity';
+import * as AdapterFactory from './adapter/index'
 
 export default {
 
   build:(el)=> {
-    let activity = new SelectionActivity(el);
-    console.log('hello world')
+    let adapter = AdapterFactory.create('table');
+    let activity = new SelectionActivity(el, adapter);
   }
 
 };
