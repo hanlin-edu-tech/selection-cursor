9c7afb378ca13104cc0a7dec925fee752debaa5f
Sparrow.jang <sparrow.jang@gmail.com>
2019-09-19 18:25:36 +0800

    Added a off method.

--- src/util/eventHelper.js
+++ src/util/eventHelper.js
@@ -1,4 +1,6 @@
 
+const registerMap = new Map();
+
 const getDelegation = (selector, target, sourceEl)=> {
   if (!selector) return null;
 
@@ -19,6 +21,38 @@ const getDelegation = (selector, target, sourceEl)=> {
   return null;
 };
 
+
+const putListener = (el, eventName, selector, func, listenerFn) => {
+
+  let listenerFns = registerMap.get(el) || [];
+
+  listenerFns.push({
+    eventName:eventName,
+    selector:selector,
+    listenerFn:listenerFn,
+    func:func
+  });
+
+  registerMap.set(el, listenerFns);
+
+};
+
+const removeListener = (el, eventName, selector, func)=> {
+
+  let listenerFns = registerMap.get(el) || [];
+
+  let index = listenerFns.findIndex((row)=> {
+    return row.eventName == eventName && row.selector == selector && row.func == func;
+  });
+
+  if (index != -1) {
+    return (listenerFns.splice(index, 1)[0]||{}).listenerFn;
+  }
+
+  return null;
+
+};
+
 const on = (...args)=> {
   let el = null, eventName = null , selector = null, func = null;
 
@@ -28,7 +62,7 @@ const on = (...args)=> {
     [el, eventName, selector, func] = args;
   }
 
-  el.addEventListener(eventName, (...eventArgs)=> {
+  let listenerFn = (...eventArgs)=> {
     let evt = eventArgs[0];
 
     let delegatedTarget = getDelegation(selector, evt.target, el);
@@ -38,11 +72,28 @@ const on = (...args)=> {
 
       func.apply(el, eventArgs);
     }
+  };
 
-  });
+  el.addEventListener(eventName, listenerFn);
+  putListener(el, eventName, selector, func, listenerFn)
+};
+
+const off = (...args)=> {
+  let el = null, eventName = null , selector = null, func = null;
+
+  if (args.length == 3) {
+    [el, eventName, func] = args;
+  } else {
+    [el, eventName, selector, func] = args;
+  }
+
+  let listenerFn = removeListener(el, eventName, selector, func);
+
+  el.removeEventListener(eventName, listenerFn);
 };
 
 export default { 
-  on:on
+  on:on,
+  off:off
 };
 
